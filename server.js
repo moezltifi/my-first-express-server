2e334d940f88c174454773d01a13435ab0422054
moezl <moez.mltifi@gmail.com>
2024-04-13 13:21:03 +0100

    Undrestanding working with route

--- server.js
+++ server.js
@@ -2,6 +2,13 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", function(req, res){
+    res.send("<h1>moez<h1><h1>moez<h1>")
+});
+app.get("/about", function(req, res){
+    res.send("<h1>I am Moez I love code <h1>")
+});
+
 app.listen(3000, function(){
     console.log("Server started on port 3000");
 })
